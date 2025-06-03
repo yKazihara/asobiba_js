8d8dfb4ad2f669302c7dffa48aacfd6de95c2e61
yKazihara <run5150@gmail.com>
2025-06-04 07:08:18 +0900

    イベントハンドラを登録するコードの記述位置に関する注意と対処方法のサンプル。

---
+++ js/dom_content_loaded.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function(){
+  let button = document.getElementById('xxx');
+  button.addEventListener('click', function(){
+    console.log('Thank you');
+  });
+});
---
+++ js/load.js
@@ -0,0 +1,6 @@
+window.onload = function(){
+  let button = document.getElementById('xxx');
+  button.onclick = function(){
+    console.log('How are you?');
+  };
+};
